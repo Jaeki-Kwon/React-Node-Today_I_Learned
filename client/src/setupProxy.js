6235519d7cafec5c8feab54f0849bde3f2362408
Jaeki-Kwon <worl1126@naver.com>
2021-07-24 01:36:56 +0900

    make it better

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,18 +1,11 @@
-// const { createProxyMiddleware } = require("http-proxy-middleware");
-
-// module.exports = function (app) {
-//   app.use(
-//     "/api",
-//     createProxyMiddleware({
-//       target: "http://localhost:5000",
-//       changeOrigin: true,
-//     })
-//   );
-// };
-
-const proxy = require("http-proxy-middleware");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
-  // add other server routes to path array
-  app.use(proxy(["/api"], { target: "http://localhost:5000" }));
+  app.use(
+    "/api",
+    createProxyMiddleware({
+      target: "http://localhost:5000",
+      changeOrigin: true,
+    })
+  );
 };
