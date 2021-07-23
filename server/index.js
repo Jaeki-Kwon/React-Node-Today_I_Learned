9e74885db315d285a5dd38f689a5744ccf89daf7
Jaeki-Kwon <worl1126@naver.com>
2021-07-23 20:55:13 +0900

    heroku

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const PORT = 5000;
 
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
@@ -27,6 +26,23 @@ app.use(cookieParser());
 
 app.use("/api/users", require("./routes/users"));
 app.use("/api/board", require("./routes/board"));
+
+//use this to show the image you have in node js server to client (react js)
+//https://stackoverflow.com/questions/48914987/send-image-path-from-node-js-express-server-to-react-client
+app.use("/uploads", express.static("uploads"));
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  // index.html for all page routes
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
 
 const handleListening = () =>
   console.log(`✅  Listening on: http://localhost:${PORT}`);
